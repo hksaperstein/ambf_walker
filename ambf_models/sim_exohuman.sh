fcddbe9a1c6c0257d58e32f37d74b799c704fdce
Harrison Saperstein <harrison.saperstein@gmail.com>
2020-09-24 19:46:46 -0400

    hks specific paths

--- ambf_models/sim_exohuman.sh
+++ ambf_models/sim_exohuman.sh
@@ -1,4 +1,4 @@
 source ~/.bashrc
-source ~/ambf/build/devel/setup.bash
-cd ~/ambf/bin/lin-x86_64
-./ambf_simulator -a ~/catkin_ws/src/ambf_walker/ambf_models/simulator/exohuman.yaml
+#source ~/ambf/build/devel/setup.bash
+cd ~/WPI/thesis/git/ambf/bin/lin-x86_64
+./ambf_simulator -a ~/WPI/thesis/git/catkin_ws/src/ambf_walker/ambf_models/simulator/exohuman.yaml
