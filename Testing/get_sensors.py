11b923704e90fed649d668c554559a2b8608123d
nathaniel <nagoldfarb@wpi.edu>
2020-10-22 14:44:26 -0400

    added sensors
    add the sensors to the model. Need to add the callbacks for the point cloud

--- Testing/get_sensors.py
+++ Testing/get_sensors.py
@@ -9,26 +9,31 @@ client = Client()
 client.connect()
 
 sensor = client.get_obj_handle("/ambf/env/Prox")
-rospy.sleep(2)
+rospy.sleep(3)
 pub = rospy.Publisher('cloud', PointCloud, queue_size=10)
 rate = rospy.Rate(1000)
-theta = np.linspace(0, 2*np.pi, 20)
+theta = np.linspace(0, 2*np.pi, 10)
 
 while 1:
 
-    ranges = sensor.get_all_measurement()
+    ranges = sensor.get_all_measurements()
     range = sensor.get_range(0)
-    print(ranges)
+    print(range)
+
+    parent = sensor.get_parent()
+    print(parent)
+
+    pose = sensor.get_pose()
+    print(pose)
 
     msg = PointCloud()
-    msg.header.stamp = rospy.Time.now();
+    msg.header.stamp = rospy.Time.now()
     msg.header.frame_id = "/stair"
-
+    #print(range)
     for r, angle in zip(ranges, theta):
         # if r == 0.0:
         #     r = 3.5
         point = Point32()
-        print(r)
         point.x = (1.5-r) * np.cos(angle)
         point.y = (1.5-r) * np.sin(angle)
         point.z = 0.2
--- Testing/make_sensors.py
+++ Testing/make_sensors.py
@@ -3,12 +3,12 @@ import numpy as np
 
 raduis = 1
 
-theta = np.linspace(0, 2*np.pi, 20)
+theta = np.linspace(-np.pi, -0.5*np.pi, 40)
 
 for angle in theta:
     x = np.round(raduis * np.cos(angle),3)
     y = np.round(raduis * np.sin(angle),3)
 
-    print("- offset: {x: 0.0, y: 0.4, z: 0.0}")
-    print("  direction: {x: " +  str(x) + ", y: 0.0" + ",z: " + str(y) + "}" )
+    print("- offset: {x: 0.0, y: 0.0, z: 0.0}")
+    print("  direction: {x: 0.0"  + ", y: " + str(x) + ",z: " + str(y) + "}" )
     print("")
