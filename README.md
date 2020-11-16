357b0c20709a2ffce273af809e8560925cc0c473
Harrison Saperstein <harrison.saperstein@gmail.com>
2020-11-16 13:57:23 -0500

    update v4 to be env var dependant need to update other simulator

---
+++ README.md
@@ -0,0 +1,3 @@
+TO run simulators:
+    create env var AMBF_WALKER_ABS with absolute <path_to_ambf_walker>
+    
--- ambf_models/v4/exohuman_v4.sh
+++ ambf_models/v4/exohuman_v4.sh
@@ -1,4 +1,4 @@
 source ~/.bashrc
 #source ~/ambf/build/devel/setup.bash
 cd ~/WPI/thesis/git/ambf/bin/lin-x86_64
-./ambf_simulator -a ~/WPI/thesis/git/catkin_ws/src/ambf_walker/ambf_models/simulator/v4/exohuman_v4.yaml -t 1 -p 60
+./ambf_simulator -a ${AMBF_WALKER_ABS}/ambf_models/v4/exohuman_v4.yaml -t 1 -p 60
