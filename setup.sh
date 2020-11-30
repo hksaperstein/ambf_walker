5aac181d286cef62871235dc94644818b1217422
Nathaniel <nagoldfarb@wpi.edu>
2020-11-30 16:25:26 -0500

    optmization of setup

--- setup.sh
+++ setup.sh
@@ -11,11 +11,4 @@ cd ..
 # shellcheck disable=SC2164
 git clone https://github.com/WPI-AIM-Hybrid-Exoskeleton/rbdl-orb.git
 # shellcheck disable=SC2164
-cd rbdl-orb
-mkdir rbdl-build
-cd rbdl-build/ || exit
-cmake -D RBDL_BUILD_PYTHON_WRAPPER=True CMAKE_BUILD_TYPE=Release ../
-make
-sudo make install
-cd ./python
-sudo cp rbdl.so /usr/local/lib/python3.8/dist-packages
+cd rbdl-orb && sh setup.sh
