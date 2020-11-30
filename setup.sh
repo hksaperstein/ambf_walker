b980ac81905747c9806309bd951bd3a13ce90b4f
Nathaniel <nagoldfarb@wpi.edu>
2020-11-30 16:17:35 -0500

    minor setup fixed

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 sudo apt install libeigen3-dev
 sudo apt install ros-melodic-smach
-pip3 install git+https://github.com/WPI-AIM/AIM_GaitAnalysisToolkit.gi
+pip3 install git+https://github.com/WPI-AIM/AIM_GaitAnalysisToolkit.git
 
 # shellcheck disable=SC2164
 cd lib
 git clone https://github.com/WPI-AIM-Hybrid-Exoskeleton/ilqr.git
-cd ilqr && python setup.py install
+cd ilqr && python3 setup.py install
 cd ..
+
 # shellcheck disable=SC2164
 git clone https://github.com/WPI-AIM-Hybrid-Exoskeleton/rbdl-orb.git
 # shellcheck disable=SC2164
