23d26fa1413f3711934166d63e73ba0b805d8a46
Nathaniel <nagoldfarb@wpi.edu>
2020-11-30 15:55:06 -0500

    working on setup file

---
+++ setup.sh
@@ -0,0 +1,20 @@
+sudo apt install libeigen3-dev
+sudo apt install ros-melodic-smach
+pip3 install git+https://github.com/WPI-AIM/AIM_GaitAnalysisToolkit.gi
+
+# shellcheck disable=SC2164
+cd lib
+git clone https://github.com/WPI-AIM-Hybrid-Exoskeleton/ilqr.git
+cd ilqr && python setup.py install
+cd ..
+# shellcheck disable=SC2164
+git clone https://github.com/ORB-HD/rbdl-orb.git
+# shellcheck disable=SC2164
+cd rbdl-orb
+mkdir rbdl-build
+cd rbdl-build/ || exit
+cmake -D RBDL_BUILD_PYTHON_WRAPPER=True CMAKE_BUILD_TYPE=Release ../
+make
+sudo make install
+cd ./python
+sudo cp rbdl.so /usr/local/lib/python3.8/dist-packages
