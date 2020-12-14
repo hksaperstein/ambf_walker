0db8c89746daaf907269d26ff098e766478e9f60
Harrison Saperstein <harrison.saperstein@gmail.com>
2020-12-14 16:19:49 -0500

    changed ambf path, added exo human walk file

---
+++ ambf_models/exohuman_v4/exohuman_v4.sh
@@ -0,0 +1,4 @@
+source ~/.bashrc
+#source ~/ambf/build/devel/setup.bash
+cd ~/WPI/thesis/git/ambf/bin/lin-x86_64
+./ambf_simulator -a ${AMBF_WALKER}/ambf_models/exohuman_v4/exohuman_v4.yaml -t 1 -p 60
